d8df41cbc03fc41acd8809a0452439bdfb9e6f75
Simon Fernandez <simon.fernandez@ens-lyon.fr>
2018-03-28 14:21:06 +0200

    Delete tester, clean the functions

--- tester.erl
+++
@@ -1,9 +0,0 @@
--module(tester).
--export([sendCommand/3, sendMsg/3]).
-
-sendCommand(Entry, Target, {Module, Fun, Args}) ->
-	{listener, Entry} ! {command, {Target, Module, Fun, Args}, rand:uniform(4096)}.
-
-sendMsg(Entry, Target, Msg) ->
-	{listener, Entry} ! {command, {Target, io, fwrite, [Msg, []]}, rand:uniform(4096)}.
-
